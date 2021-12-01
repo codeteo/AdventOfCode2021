b80f7d74d03e8e3987048f57e99a1249c13f2ac5
tioakeimidis <tioakeimidis@trading-point.com>
2021-12-01 22:41:33 +0200

    Solution for Day 1 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,8 +1,31 @@
 fun main() {
-    val stringLines: List<String> = readInput("inputs/Day01_input")
-    var lines: List<Int> = stringLines.map { it.toInt() }
+    part1()
+    part2()
+}
 
-    var prevLine = lines[0] // default is first
+private fun part2() {
+    val stringLines: List<String> = readInput("inputs/Day01_input")
+    val lines: List<Int> = stringLines.map { it.toInt() }
+
+    var totalIncreased = 0
+    for ((index, _) in lines.withIndex()) {
+        if (index == lines.size - 3) break
+
+        val first = lines[index] + lines[index + 1] + lines[index + 2]
+        val second = lines[index + 1] + lines[index + 2] + lines[index + 3]
+
+        if (second > first) {
+            totalIncreased++
+        }
+    }
+    println("Result Part 2 = $totalIncreased")
+}
+
+private fun part1() {
+    val stringLines: List<String> = readInput("inputs/Day01_input")
+    val lines: List<Int> = stringLines.map { it.toInt() }
+
+    var prevLine = lines[0]
     var totalIncreased = 0
     for (line in lines.drop(1)) {
         if (line > prevLine) {
@@ -11,5 +34,5 @@ fun main() {
         prevLine = line
     }
 
-    println("Result = $totalIncreased")
+    println("Result Part 1 = $totalIncreased")
 }
