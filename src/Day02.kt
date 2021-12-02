de61e350082d18854c626c1265ceef75649de096
tioakeimidis <tioakeimidis@trading-point.com>
2021-12-02 20:49:36 +0200

    Solves Day 2 Part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,7 +2,34 @@ import Action.*
 
 fun main() {
     part1()
+    part2()
 }
+
+private fun part2() {
+    val lines: List<String> = readInput("inputs/Day02_input")
+
+    val position = PositionWithAim(0, 0, 0)
+
+    for (line in lines) {
+        val value = line.retrieveValue()
+        when (line.toAction()) {
+            DOWN -> {
+                position.aim += value
+            }
+            UP -> {
+                position.aim -= value
+            }
+            FORWARD -> {
+                position.horizontal += value
+                position.depth += position.aim * value
+            }
+        }
+    }
+
+    println("Result for Part 2 is: ${position.horizontal * position.depth}")
+}
+
+data class PositionWithAim(var horizontal: Int, var depth: Int, var aim: Int)
 
 private fun part1() {
     val lines: List<String> = readInput("inputs/Day02_input")
@@ -18,7 +45,7 @@ private fun part1() {
         }
     }
 
-    println("Result is ${position.multiply}")
+    println("Result for Part 1 is: ${position.multiply}")
 }
 
 data class Position(var horizontal: Int, var depth: Int)
